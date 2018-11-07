e9e74b24cf42e0a9644a589d286d92c7a80bacee
Filip Andrei <filip@graffino.com>
2018-11-07 22:21:02 +0200

    Add vue file for initializing library

---
+++ src/components/ItunesLibrary.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'ItunesLibrary',
+  mounted() {
+    this.readItunesLibrary();
+  },
+  methods: {
+    ...mapActions([
+      'readItunesLibrary',
+    ])
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="iTunes logo" src="../assets/itunes.png">
     <Search/>
+    <ItunesLibrary/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Search from '@/components/Search.vue';
+import ItunesLibrary from '@/components/ItunesLibrary.vue';
 
 export default {
   name: 'home',
   components: {
     Search,
+    ItunesLibrary
   },
 };
 </script>
