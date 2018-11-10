10f2461e66b2cd676d49a0cb9dfaf978430a37be
Filip Andrei <filip@graffino.com>
2018-11-10 14:43:31 +0200

    Add getters for genres and songs

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,12 +1,22 @@
 export const getters = {
+  genres(state) {
+    const allGenres = state.library.map(song => song.genre);
+    const uniqueGenres = [...new Set(allGenres)];
+    return uniqueGenres.sort();
+  },
   artists(state) {
-    const allArtists = state.songs.map(song => song.artist);
+    const allArtists = state.library.map(song => song.artist);
     const uniqueArtists = [...new Set(allArtists)];
     return uniqueArtists.sort();
   },
   albums(state) {
-    const allAlbums = state.songs.map(song => song.album);
+    const allAlbums = state.library.map(song => song.album);
     const uniqueAlbums = [...new Set(allAlbums)];
     return uniqueAlbums.sort();
+  },
+  songs(state) {
+    const allSongs = state.library.map(song => song.title);
+    const uniqueSongs = [...new Set(allSongs)];
+    return uniqueSongs.sort();
   }
 };
